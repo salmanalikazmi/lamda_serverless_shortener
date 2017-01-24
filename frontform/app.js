e386ab17d1c2c9725e3a98854dc1d94d30232062
CYBER-TIGER\Salman <salmanalikazmi@gamil.com>
2017-01-25 02:09:21 +0800

    updates on front end

--- frontform/app.js
+++ frontform/app.js
@@ -1,5 +1,8 @@
 $(function() {
-
+//hash genrator	
+function generateUIDNotMoreThan1million() {
+    return ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4)
+}
 	// Get the form.
 	var form = $('#ajax-contact');
 
@@ -14,7 +17,7 @@ $(function() {
 		// Serialize the form data.
 		//var formData = $(form).serialize();
 		var longurl = $(form).find('input[name="url"]').val();
-
+		var short = generateUIDNotMoreThan1million();
 		//ajax setup for headers
 //$.ajaxSetup({
   // headers: { "x-api-key": "Iy4W7wpmg72P4XS5wUWtC6R2uJORqhZa49FCxclo" }
@@ -29,7 +32,7 @@ var posturl = "https://mir1ws0chd.execute-api.ap-southeast-1.amazonaws.com/prod/
 			dataType : "json",
 			crossDomain: true,
         	contentType: 'application/json',
-			data: JSON.stringify( {token: "A12345", url: longurl}),			     
+			data: JSON.stringify( {token: short, url: longurl}),			     
 		})
 		//.beforeSend(function(xhr){
              //xhr.setRequestHeader('x-api-key', 'Iy4W7wpmg72P4XS5wUWtC6R2uJORqhZa49FCxclo');
@@ -38,10 +41,13 @@ var posturl = "https://mir1ws0chd.execute-api.ap-southeast-1.amazonaws.com/prod/
 			// Make sure that the formMessages div has the 'success' class.
 			$(formMessages).removeClass('error');
 			$(formMessages).addClass('success');
-
+			if (response != undefined ){
+			var res = JSON.stringify(response);
+			res = JSON.parse(res);
 			// Set the message text.
-			$(formMessages).text(response.token);
-
+			var redirURL = res.ShortURL
+			$(formMessages).append ("The short url is here, Click the link to test :<a href='" + posturl + redirURL + "'>" + redirURL + "</a>" );
+			}
 			// Clear the form.
 			$('#url').val('');			
 		})
@@ -54,7 +60,7 @@ var posturl = "https://mir1ws0chd.execute-api.ap-southeast-1.amazonaws.com/prod/
 			if (data.responseText !== '') {
 				$(formMessages).text(data.responseText);
 			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+				$(formMessages).text('Oops! An error occured and your URL could not be processed.');
 			}
 		});
 
